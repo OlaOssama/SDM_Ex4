1f0802ed1fef88ecb0d2847ff99f62013171379f
Gasiorowski <tomekgasiorowski@hotmail.com>
2017-11-17 12:40:07 +0100

    projection

--- SDM_Exercise_04_2017/src/de/tuda/sdm/dmdb/sql/operator/exercise/Projection.java
+++ SDM_Exercise_04_2017/src/de/tuda/sdm/dmdb/sql/operator/exercise/Projection.java
@@ -6,27 +6,35 @@ import de.tuda.sdm.dmdb.sql.operator.Operator;
 import de.tuda.sdm.dmdb.sql.operator.ProjectionBase;
 import de.tuda.sdm.dmdb.sql.operator.UnaryOperator;
 import de.tuda.sdm.dmdb.storage.AbstractRecord;
+import de.tuda.sdm.dmdb.storage.Record;
 
 @SuppressWarnings("unused")
 public class Projection extends ProjectionBase {
-	
+
 	public Projection(Operator child, Vector<Integer> attributes) {
 		super(child, attributes);
 	}
 
 	@Override
 	public void open() {
-		//TODO: implement this method
+		child.open();
 	}
-	
+
 	@Override
 	public AbstractRecord next() {
-		//TODO: implement this method
+		AbstractRecord ar = child.next();
+		if (ar != null) {
+			AbstractRecord result = new Record(attributes.size());
+			for (int i = 0; i < attributes.size(); i++) {
+				result.setValue(i, ar.getValue(attributes.get(i)));
+			}
+			return result;
+		}
 		return null;
 	}
-	
+
 	@Override
 	public void close() {
-		//TODO: implement this method
+		child.close();
 	}
 }
