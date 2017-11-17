fee3feccbfbb3b783d14944bf97362827193e7a4
Gasiorowski <tomekgasiorowski@hotmail.com>
2017-11-17 12:53:43 +0100

    finished??????????????????????????????

--- SDM_Exercise_04_2017/src/de/tuda/sdm/dmdb/sql/operator/exercise/EquiJoin.java
+++ SDM_Exercise_04_2017/src/de/tuda/sdm/dmdb/sql/operator/exercise/EquiJoin.java
@@ -3,27 +3,44 @@ package de.tuda.sdm.dmdb.sql.operator.exercise;
 import de.tuda.sdm.dmdb.sql.operator.EquiJoinBase;
 import de.tuda.sdm.dmdb.sql.operator.Operator;
 import de.tuda.sdm.dmdb.storage.AbstractRecord;
+import de.tuda.sdm.dmdb.storage.Record;
 
 public class EquiJoin extends EquiJoinBase {
-	
+
 	public EquiJoin(Operator leftChild, Operator rightChild, int leftAtt, int rightAtt) {
 		super(leftChild, rightChild, leftAtt, rightAtt);
 	}
-	
+
 	@Override
 	public void open() {
-		//TODO: implement this method
+		leftChild.open();
+		rightChild.open();
 	}
 
 	@Override
 	public AbstractRecord next() {
-		//TODO: implement this method
+		AbstractRecord left = leftChild.next();
+		AbstractRecord right = rightChild.next();
+		if (left != null && right != null) {
+			if (left.getValue(leftAtt).compareTo(right.getValue(rightAtt)) == 0) {
+				AbstractRecord result = new Record(left.getValues().length + right.getValues().length);
+				int i = 0;
+				for (; i < left.getValues().length; i++) {
+					result.setValue(i, left.getValue(i));
+				}
+				for (; i < right.getValues().length; i++) {
+					result.setValue(i, right.getValue(i-left.getValues().length));
+				}
+				return result;
+			}
+		}
 		return null;
 	}
-	
+
 	@Override
 	public void close() {
-		//TODO: implement this method
+		leftChild.close();
+		rightChild.close();
 	}
 
 }
