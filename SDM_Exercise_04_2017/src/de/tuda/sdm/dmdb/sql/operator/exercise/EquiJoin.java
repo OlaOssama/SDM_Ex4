5d73539dd854b8eeab1f37872824502022cd5165
Gasiorowski <tomekgasiorowski@hotmail.com>
2017-11-17 18:03:45 +0100

    open

--- SDM_Exercise_04_2017/src/de/tuda/sdm/dmdb/sql/operator/exercise/EquiJoin.java
+++ SDM_Exercise_04_2017/src/de/tuda/sdm/dmdb/sql/operator/exercise/EquiJoin.java
@@ -20,20 +20,28 @@ public class EquiJoin extends EquiJoinBase {
 	@Override
 	public AbstractRecord next() {
 		AbstractRecord left = leftChild.next();
+		rightChild.open();
 		AbstractRecord right = rightChild.next();
-		if (left != null && right != null) {
-			if (left.getValue(leftAtt).compareTo(right.getValue(rightAtt)) == 0) {
+		// System.out.println("left value(0) "+left.getValue(0));
+		// System.out.println("right value(0) "+right.getValue(0));
+
+		do {
+			if (left != null && (right != null)) {
 				AbstractRecord result = new Record(left.getValues().length + right.getValues().length);
-				int i = 0;
-				for (; i < left.getValues().length; i++) {
-					result.setValue(i, left.getValue(i));
+				if (left.getValue(leftAtt).compareTo(right.getValue(rightAtt)) == 0) {
+					int i = 0;
+					for (; i < left.getValues().length; i++) {
+						result.setValue(i, left.getValue(i));
+					}
+					for (; i < right.getValues().length; i++) {
+						result.setValue(i, right.getValue(i - left.getValues().length));
+					}
+
+					return result;
 				}
-				for (; i < right.getValues().length; i++) {
-					result.setValue(i, right.getValue(i-left.getValues().length));
-				}
-				return result;
 			}
-		}
+			right = rightChild.next();
+		} while (right != null);
 		return null;
 	}
 
